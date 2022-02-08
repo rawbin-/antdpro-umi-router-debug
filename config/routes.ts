6b9e490de7aee755a7e6cba12628ad99ca504f0b
rawbin.zhang <rawbin.zhang@lunarax.io>
2022-02-08 13:18:39 +0800

    fix: 解决菜单重定向子菜单不渲染的问题

--- config/routes.ts
+++ config/routes.ts
@@ -2,7 +2,14 @@
   {
     path: '/menu1',
     name: '菜单分类一',
+    // redirect: '/menu1/sub1', // 加了这个所有子页面都不渲染了
     routes: [
+      {
+        path: 'menu1', // 这个是用来模拟重定向的
+        name: '菜单分类一',
+        component: './menu1/menu1sub1/menu1sub1list.tsx',
+        hideInMenu: true,
+      },
       {
         path: '/menu1/sub1',
         name: '菜单分类一功能一列表',
